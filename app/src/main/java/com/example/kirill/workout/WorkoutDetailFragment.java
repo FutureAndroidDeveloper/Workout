17993aaf8ff9f148e6e3ec7930889412aec4b20e
Kirill Klimenkov <dev.kirill.klimenkov@gmail.com>
2018-11-07 14:50:46 +0300

    Override onStart() in fragment

--- app/src/main/java/com/example/kirill/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/example/kirill/workout/WorkoutDetailFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -27,6 +28,21 @@ public class WorkoutDetailFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
     }
 
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+
+        if (view != null) {
+            Workout workout = Workout.workouts[(int) workoutId];
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+
+            title.setText(workout.getName());
+            description.setText(workout.getDescription());
+        }
+    }
 
     public void setWorkoutId(long id) {
         this.workoutId = id;
