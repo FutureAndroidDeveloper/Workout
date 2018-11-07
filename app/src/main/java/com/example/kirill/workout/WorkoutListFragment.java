920a82fcfd776d4075804700af0f808b2c48d52e
Kirill Klimenkov <dev.kirill.klimenkov@gmail.com>
2018-11-07 15:09:10 +0300

    Add ListFragment

---
+++ app/src/main/java/com/example/kirill/workout/WorkoutListFragment.java
@@ -0,0 +1,28 @@
+package com.example.kirill.workout;
+
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link ListFragment} subclass.
+ */
+public class WorkoutListFragment extends ListFragment {
+
+
+    public WorkoutListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
